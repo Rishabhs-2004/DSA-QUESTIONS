90758b4bc3fd6318bd5c3cc42c7d278cbf1dfdba
Rishabh Singh <rishabhsingh9501@gmail.com>
2026-01-07 16:25:48 -0800

    7-1-26

---
+++ Reverse.java
@@ -0,0 +1,30 @@
+public class Reverse {
+    
+public static void reverse(int num[]){
+     int start = 0, end = num.length-1;
+
+     while (start < end ){
+        int temp = num[end];
+        num[end] = num[start];
+        num[start] = temp;
+
+
+
+        start ++;
+        end--;
+     }
+}
+
+    public static void main(String[] args) {
+        int num[] = {5,6,3,4,9,10};
+        
+        reverse(num);
+        for(int i=0; i<num.length; i++){
+            System.out.print("Array is reverse: " +num[i] );
+        }
+        System.out.println();
+    }
+
+
+    
+}
